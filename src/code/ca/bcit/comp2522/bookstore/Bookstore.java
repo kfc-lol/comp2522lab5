6e352b659165106328a090d2c619ee7d8c8fb49c
kfc-lol <kianfbcastro@gmail.com>
2026-02-10 16:33:04 -0800

    created list variable

--- src/code/ca/bcit/comp2522/bookstore/Bookstore.java
+++ src/code/ca/bcit/comp2522/bookstore/Bookstore.java
@@ -1,5 +1,24 @@
 package ca.bcit.comp2522.bookstore;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bookstore
 {
+
+    private final String name;
+    private final List<Novel> references;
+
+    public Bookstore(final String name)
+    {
+        this.name = name;
+        this.references = new ArrayList<>();
+
+        final Novel taoam;
+        taoam = new Novel("The Adventures of Augie March",
+                       "Saul Bellow",
+                       1953);
+
+        references.add(taoam);
+    }
 }
